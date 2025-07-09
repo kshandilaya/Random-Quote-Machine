e900dc51eb8096314bd9bbf7c78c21f8639748f8
kshandilaya <kshandilaya@gmail.com>
2025-07-09 22:26:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+#root {
+  width: 500px ;
+  height:200px;
+  margin: 0 auto;
+  padding: 3rem;
+  text-align: center;
+  border-radius: .75rem;
+  background: wheat;
+  color:black;
+}
+
+#author{
+  text-align: right;
+
+}
+
+#tweet-quote img{
+  width:40px;
+  height:40px;
+}
+
+#block{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin:10px 8px;
+}
